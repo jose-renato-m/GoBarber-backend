a23ad49a261e1936247fbbf834b7302172f698d4
José Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-08-30 16:23:10 -0300

    start rewrite repositories

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import AuthenticateUsersService from '../services/AuthenticateUserService';
+import AuthenticateUsersService from '@modules/users/services/AuthenticateUserService';
 
 const sessionsRouter = Router();
 
