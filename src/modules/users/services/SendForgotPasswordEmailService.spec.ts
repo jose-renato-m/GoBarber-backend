28e8bfff27a3244ee709759f8c81e97e6a931be0
José Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-10-17 20:02:55 -0300

    feature:start create password recover application

---
+++ src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -0,0 +1,18 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import SendForgotPasswordEmailService from './SendForgotPasswordEmailService';
+
+describe('SendForgotPasswordEmail', () => {
+  it('should be able to recover the password using the email', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+
+    const createUser = new SendForgotPasswordEmailService(fakeUsersRepository);
+
+    await createUser.execute({
+      email: 'johndoe@example.com',
+    });
+
+    expect(user).toHaveProperty('id');
+  });
+});
